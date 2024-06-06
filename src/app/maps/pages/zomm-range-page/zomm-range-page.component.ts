4a09146548dfa429dfca0293d4b302263a712731
Simon Rojas <sdavid.rojas@hotmail.com>
2024-06-06 12:37:38 -0500

    Diseño del ZoomIn y ZoomOut

--- src/app/maps/pages/zomm-range-page/zomm-range-page.component.ts
+++ src/app/maps/pages/zomm-range-page/zomm-range-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zomm-range-page.component.html',
-  styles: ``
+  styleUrl: './zomm-range-page.component.css',
 })
-export class ZommRangePageComponent {
+export class ZommRangePageComponent implements AfterViewInit{
+
+    // Permite tomar referencias del html
+    @ViewChild('map') divMap?: ElementRef; 
+
+    ngAfterViewInit(): void {
+  
+      if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+  
+      const map = new Map({
+        container: this.divMap.nativeElement, // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+    }
 
 }
