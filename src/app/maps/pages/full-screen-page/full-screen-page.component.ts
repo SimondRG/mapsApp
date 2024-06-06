69bb8bd37f288d3e4575f1352c1af1bb6946fa4c
Simon Rojas <sdavid.rojas@hotmail.com>
2024-06-05 22:06:32 -0500

    Configuración de MapBox para mostrar el mapa en pantalla completa, el en componente de full-screen

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2ltb25yZyIsImEiOiJjbHgyZ3R0NDAwa2NyMmtxNmNnam00dGk1In0.gJw5BBCNeo3gvHJYW-KQig';
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styles: ``
+  styleUrl: './full-screen-page.component.css',
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit{
+
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+  
 
 }
