8c2b93a0dae47ed764aa417456a7ee5885f45e6b
Simon Rojas <sdavid.rojas@hotmail.com>
2024-06-07 12:19:54 -0500

    Implementación del componente standalone counter-alone dentro del componente standalone alone-page, como un componente reutilizable

--- src/app/alone/page/alone-page/alone-page.component.ts
+++ src/app/alone/page/alone-page/alone-page.component.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
-  //imports: [CommonModule],
+  imports: [ CounterAloneComponent, ], // Importa el counterAlone para utilizarlo dentro de este componente
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
 })
