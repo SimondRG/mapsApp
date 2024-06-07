bcc46b899332594f7f0992fdc1897a855ff590f0
Simon Rojas <sdavid.rojas@hotmail.com>
2024-06-06 21:28:57 -0500

    Creación de las propiedades del componente properties-page y maquetación del template, creación de la interface House, desarrollo del componente reurilizable mini-map, que recibe la información de las coordenadas desde el componente padre propeties-page y se renderiza dentro de este último componente, para mostrar los mini mapas

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl'; // Se llama este paquete preinstalado por npm para el manejo de mapas
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: ``
+  styleUrl: './mini-map.component.css',
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+  
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?:Map;
+  
+  ngAfterViewInit(): void {
+    
+    if( !this.divMap?.nativeElement ) throw 'Map Div not found';
+    if( !this.lngLat ) throw "LngLat can't be null";
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    new Marker()
+      .setLngLat( this.lngLat ) // Se establecen las coordenadas
+      .addTo( this.map ); // Se añade al mapa actual
+
+  }
+
 
 }
